c9532bd19ffaeb28d7ea9581c5e69d4c4544b669
vcboy <yab.shi@gmail.com>
2019-10-09 17:08:45 +0800

    add python webstock client

--- runcode/getlanip.py
+++ runcode/getlanip.py
@@ -1,6 +1,8 @@
 import socket
+import os
 import urllib.request
 import urllib.parse
+from websocket import create_connection
 
 def get_lan_ip():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -9,9 +11,32 @@ def get_lan_ip():
     s.close()
     return ipaddr
 
+def send_tcp_server():
+    ss = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    ss.connect(('workman-chat.mynep.com', 7272))
+    #f=open('aa','wb')
+    msg = 'tom'
+    ss.sendall(msg.encode('utf-8'))
+    #os.system('sleep 1')
+    #ss.send('FOE')
+    data = ss.recv(1024)
+    print('server dafu %s' % data.decode())
+    ss.close()
+
+def send_websocket_server():   
+    ws = create_connection("ws://127.0.0.1:2000/") # workerman的websocket服务端
+    print("Sending 'Hello, World'...")
+    ws.send("Hello, World")
+    print("Sent")
+    print("Receiving...")
+    result = ws.recv()
+    print("Received '%s'" % result)
+    ws.close()
+
 if __name__ == '__main__':
     lanip=get_lan_ip()
     print(lanip)
-    url = 'http://192.168.1.4/1.txt'
-    f = urllib.request.urlopen(url)
-    print(f.read().decode('utf-8'))
+    # url = 'http://192.168.1.4/1.txt'
+    # f = urllib.request.urlopen(url)
+    # print(f.read().decode('utf-8'))
+    send_websocket_server()
